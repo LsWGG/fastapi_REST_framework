4b618275db3b49ceefa77154ea406ef0a7d32172
lishun <lishunwss@163.com>
2024-10-28 01:09:50 +0800

    add 库依赖、自动生成CRUD所需Schema(未完成)

--- demo_service/api/base_api.py
+++ demo_service/api/base_api.py
@@ -8,6 +8,7 @@
 """
 from libs.fastapi_rest_framework.schemas import BaseResponseMode
 from libs.fastapi_rest_framework.viewsets import ModelViewSet
+from schemas import GenericSerializerSchemas
 
 from ..models import Author
 from ..schemas.demo_schema import (
@@ -26,14 +27,14 @@ class AuthorViewSet(ModelViewSet):
     """
     # 数据库模型类
     model = Author
-    # generic_schema = GenericSerializerSchemas
+    generic_schema = GenericSerializerSchemas
 
     # Schema类：请求参数校验
-    schema_class = QueryAuthorSchema
-    create_schema_class = CreateAuthorSchema
-    update_schema_class = UpdateAuthorSchema
+    # schema_class = QueryAuthorSchema
+    # create_schema_class = CreateAuthorSchema
+    # update_schema_class = UpdateAuthorSchema
 
     # 响应模型：定义响应格式
-    response_model = BaseResponseMode
-    list_response_model = ResponseListAuthorSchema
-    retrieve_response_model = ResponseAuthorSchema
+    # response_model = BaseResponseMode
+    # list_response_model = ResponseListAuthorSchema
+    # retrieve_response_model = ResponseAuthorSchema
--- demo_service/libs/fastapi_rest_framework/utils/generice_schema.py
+++ demo_service/libs/fastapi_rest_framework/utils/generice_schema.py
@@ -8,60 +8,101 @@ from ..schemas import BaseSerializerSchemas, BaseResponseMode, BaseResponseListS
 class GenericSchema:
     def __init__(self, model: BaseModel):
         self.model = model
-        self.read_only = tuple(self.get_read_only_fields())
+        self.generated_field = tuple(self.get_generated_fields())
 
-    def get_read_only_fields(self) -> list:
+    def get_generated_fields(self) -> list:
         return [
             field_name for field_name, field in self.model._meta.fields_map.items() if field.allows_generated
         ]
 
-    def generate_meta(self, is_get: bool):
+    def generate_meta(self, many: bool):
         class Meta:
             model = self.model
-            is_many = is_get
+            is_many = many
 
         return Meta
 
-    def generate_pydantic(self, is_get: bool):
-        class PydanticModel:
-            exclude = () if is_get else self.read_only
+    def generate_pydantic(self, read_only: bool):
+        class PydanticMeta:
+            exclude = () if read_only else self.generated_field
 
-        return PydanticModel
+        return PydanticMeta
 
     def generate_schema(
             self,
             suffix: str = "Schema",
-            is_get: bool = True,
-            is_schema_class: bool = True,
-            super_class: PydanticModel = Union[BaseSerializerSchemas, BaseResponseMode, BaseResponseListSchema]
+            many: bool = False,
+            read_only: bool = False,
+            super_class: PydanticModel = Union[BaseSerializerSchemas, BaseResponseMode, BaseResponseListSchema],
     ):
+        # todo: 生成有问题
         schema_name = f"{self.model.__name__}{''.join([s.capitalize() for s in suffix.split('_')])}"
         schema_bases = (super_class,)
         schema_dict = {
-            "Meta": self.generate_meta(is_get),
-            "PydanticMeta": self.generate_pydantic(is_get),
+            "__module__": self.__module__,
+            "Meta": self.generate_meta(many=many),
+            "PydanticMeta": self.generate_pydantic(read_only=read_only),
         }
-
-        schema_class = type(schema_name, schema_bases, schema_dict)
+        schema_class = type(schema_name, schema_bases, {})
+        setattr(schema_class, "Meta", self.generate_meta(many=many))
+        setattr(schema_class, "PydanticMeta", self.generate_pydantic(read_only=read_only))
         return schema_class
 
     def init_schema(self, cls):
-        schema_map = ("create", "list", "retrieve", "update", "destroy")
+        # Schema类：请求参数校验
+        cls.schema_class = self.generate_schema(
+            suffix=f"schema_class",
+            read_only=True,
+            super_class=cls.generic_schema
+        )
 
-        for _map in schema_map:
-            _class = self.generate_schema(
-                suffix=f"{_map}_schema_class",
-                is_get=True if _map in ["list", "retrieve"] else False,
-                is_schema_class=True,
-                super_class=cls.generic_schema
-            )
-            setattr(cls, f"{_map}_schema_class", _class)
+        base_schema = self.generate_schema(
+            suffix=f"base_schema",
+            super_class=cls.generic_schema
+        )
+        cls.create_schema_class = None
+        cls.update_schema_class = None
 
-        for _map in schema_map:
-            _class = self.generate_schema(
-                suffix=f"{_map}_response_model",
-                is_get=True if _map in ["list", "retrieve"] else False,
-                is_schema_class=False,
-                super_class=BaseResponseListSchema if _map in ["list"] else BaseResponseMode
-            )
-            setattr(cls, f"{_map}_response_model", _class)
+        # 响应模型：定义响应格式
+        cls.response_model = BaseResponseMode
+
+        list_base_schema = self.generate_schema(
+            suffix=f"list_base_schema",
+            many=True,
+            read_only=True,
+            super_class=cls.generic_schema
+        )
+
+        class ResponseListAuthorSchema(BaseResponseListSchema):
+            class ListData(BaseModel):
+                data: Union[None, list_base_schema()]
+                count: int
+
+            data: ListData
+
+        cls.list_response_model = ResponseListAuthorSchema
+
+        class ResponseRetrieveSchema(BaseResponseMode):
+            data: Union[None, cls.schema_class()]
+
+        cls.retrieve_response_model = ResponseRetrieveSchema
+
+        # schema_map = ("create", "list", "retrieve", "update", "destroy")
+        #
+        # for _map in schema_map:
+        #     _class = self.generate_schema(
+        #         suffix=f"{_map}_schema_class",
+        #         is_get=True if _map in ["list", "retrieve"] else False,
+        #         is_schema_class=True,
+        #         super_class=cls.generic_schema
+        #     )
+        #     setattr(cls, f"{_map}_schema_class", _class)
+        #
+        # for _map in schema_map:
+        #     _class = self.generate_schema(
+        #         suffix=f"{_map}_response_model",
+        #         is_get=True if _map in ["list", "retrieve"] else False,
+        #         is_schema_class=False,
+        #         super_class=BaseResponseListSchema if _map in ["list"] else BaseResponseMode
+        #     )
+        #     setattr(cls, f"{_map}_response_model", _class)
